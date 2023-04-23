e160201b267852a41b87b6588eae968b53d83ad9
Baher Abdelfatah <baherbeho_ls@yahoo.com>
2023-04-23 17:16:23 +0200

    Implement Factory Method

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -6,7 +6,56 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var notificationServiceProvider = new NotificationServiceProvider();
+            var shippingService = new ShippingService(notificationServiceProvider);
+            shippingService.ShipItem();
+        }
+    }
+
+    interface IUserNotifier
+    {
+        void NotifyUser(int id);
+    }
+
+    class EmailUserNotifer : IUserNotifier
+    {
+        public void NotifyUser(int id)
+        {
+            Console.WriteLine($"Notified User {id} By Email");
+        }
+    }
+
+    class TestUserNotifier : IUserNotifier
+    {
+        public void NotifyUser(int id)
+        {
+            Console.WriteLine($"Pretending to notify User {id}");
+        }
+    }
+
+    class NotificationServiceProvider
+    {
+        public IUserNotifier GetUserNotifier()
+        {
+#if DEBUG
+                return new TestUserNotifier();
+#else
+            return new EmailUserNotifer();
+#endif
+        }
+    }
+
+    class ShippingService
+    {
+        NotificationServiceProvider _serviceProvider;
+
+        public ShippingService(NotificationServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void ShipItem() {
+            _serviceProvider.GetUserNotifier().NotifyUser(1);
         }
     }
 }
