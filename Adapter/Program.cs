7ef393f1afb6d4d264334013e4f4e1001786e09c
Baher Abdelfatah <baherbeho_ls@yahoo.com>
2023-05-02 04:21:16 +0200

    Adapt FileStream to ILogger

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using Microsoft.Extensions.Logging;
+using System.Text;
 namespace Adapter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new FileLogger<Program>("Log.txt");
+            logger.LogDebug("This is a new log message");
+        }
+
+        class FileLogger<T> : FileStream, ILogger<T> {
+            public FileLogger(string path) : base(path, FileMode.Append) {}
+
+            public IDisposable BeginScope<TState>(TState state) {
+                throw;
+            }
+
+            public bool IsEnabled(LogLevel logLevel) {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception) {
+                byte[] messageByteArray = new UTF8Encoding(true).GetBytes(state.ToString() + "\n");
+
+                Write(messageByteArray, 0, messageByteArray.Length);
+                Flush();
+            }
         }
     }
 }
